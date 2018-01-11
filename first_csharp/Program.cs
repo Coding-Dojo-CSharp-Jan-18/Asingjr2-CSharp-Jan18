851145b1f816350170f93c97af40a19a826ca95e
asingjr2 <asingjr2@gmail.com>
2018-01-10 23:02:06 -0500

    starting Dojo C# stuff

---
+++ first_csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace first_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Below are basic print statements with some interpolation
+            Console.WriteLine("Hello All Worlds!");
+            // Interger variable define and used
+            int testnum = 1001;
+            Console.WriteLine("the following is an int var testnum", testnum);
+            Console.WriteLine(testnum);
+            Console.WriteLine("Writing in double quotations to practice string interpolation with brackets to write numbers like {0}", testnum);
+            Console.WriteLine("Checking interpolation using two numbers and one string within WriteLines(), {0}{1}{2}", 1,2, "3");
+            string mathy = $"Trying use of math functions in strings with 3 plus 6 equaling {3+6}, 4 multiplied by 1012 equaling {4*1012}";
+            Console.WriteLine(mathy);
+            Console.WriteLine($"{5*4} was created by inserting 4 * 5 into brackets within quotations as a string prefaced by the $ sign");
+            // Trying basic for loop
+            for (int i =1; i < 6; i++){
+                Console.WriteLine("writing increasing variable");
+                Console.WriteLine(i);
+            }
+            // Trying basic while loop
+            int l = 4;
+            while (l > 0){
+                Console.WriteLine("Increasing variable with while loop");
+                Console.WriteLine(l);
+                l--;
+            }
+            // Trying random, rand.Next(), rand.Next(Int32), rand.Next(Int32, Int32), rand.NextDouble()
+            Random rand = new Random();
+            for (int a = 0; a < 4; a++){
+                Console.WriteLine("Random integer followed by standard interger");
+                Console.WriteLine(rand.Next(2,50));
+                int thing = rand.Next(10); 
+                Console.WriteLine(thing);
+            }
+
+        }
+    }
+}
---
+++ fundamentals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fundamentals Assignment");
+            Console.WriteLine("Loop for 1 to 255");
+            for (int i =1; i<256;i++){
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Loop for 1 to 100 divisible by 3, 5, and both");
+            for (int a=1; a<101; a++){
+                if (a % 3 == 0){ Console.WriteLine("Fizz");}
+                if (a % 5 == 0) { Console.WriteLine("Buzz");}
+                if (a % 15== 00) { Console.WriteLine("FizzBuzz"); }
+            }
+            Console.WriteLine("Loop for 1 to 100 divisible by 3 and 5 but not both");
+                for (int a=1; a<100; a++){
+                    if (a % 3 == 0 && a%5 !=0){ Console.WriteLine("Fizz");}
+                    if (a % 5 == 0 && a % 3 != 0) { Console.WriteLine("Buzz"); }
+                }
+            Console.Write("Loop for ten random numbers divisible by 3 or 5");
+            Random rand = new Random();
+
+            for (int i = 1; i<11; i++){  
+                int value = rand.Next(101);
+                
+                if (value % 3 == 0 && value % 5 != 0) { Console.WriteLine($"Fizz {value}"); }
+                if (value % 5 == 0 && value % 3 != 0) { Console.WriteLine($"Buzz {value}"); }
+            }
+        }
+    }
+}
