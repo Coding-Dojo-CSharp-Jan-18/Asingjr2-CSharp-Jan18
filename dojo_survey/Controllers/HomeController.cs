812f381950af80d6af387d2f66a6acf5ebed7484
asingjr2 <asingjr2@gmail.com>
2018-01-25 23:38:18 -0500

    adding c mvc session

--- dojo_survey/Controllers/HomeController.cs
+++ dojo_survey/Controllers/HomeController.cs
@@ -13,15 +13,36 @@ namespace dojo_survey.Controllers
         {
             return View();
         }
+        // [HttpGet("Post")]
+        // public IActionResult Post(string name, string location, string langauge, string comment)
+        // {
+        //     ViewBag.Name = name;
+        //     ViewBag.Location = location;
+        //     ViewBag.Language = langauge; 
+        //     ViewBag.Comment = comment;
+        //     return View("Results");
+        // }
+
+        // Code refractored to allow for the passing of URL params
         [HttpGet("Post")]
         public IActionResult Post(string name, string location, string langauge, string comment)
         {
+            return RedirectToAction("Results", new{
+                // Can define types later
+                name = name, 
+                location = location, 
+                langauge = langauge,
+                comment = comment
+            });
+        }
+        [HttpGet("Results")]
+        public IActionResult Results(string name, string location, string langauge, string comment)
+        {
             ViewBag.Name = name;
             ViewBag.Location = location;
-            ViewBag.Language = langauge; 
+            ViewBag.Language = langauge;
             ViewBag.Comment = comment;
-            return View("Results");
+            return View();
         }
-
     }
 }
