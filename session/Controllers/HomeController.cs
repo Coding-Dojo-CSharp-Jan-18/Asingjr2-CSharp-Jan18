5df9b0e5bccdbd8d9e0327778e165dbc4de1401d
asingjr2 <asingjr2@gmail.com>
2018-01-26 12:52:31 -0500

    adding simple session

---
+++ session/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace session.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            System.Console.WriteLine("Index page sir");
+            HttpContext.Session.SetString("UserName", "Genevieve");
+            string gen = HttpContext.Session.GetString("UserName");
+            ViewBag.Gen = gen;
+            HttpContext.Session.SetInt32("Age", 50);
+            int? age_num = HttpContext.Session.GetInt32("Age");
+            ViewBag.Age = age_num;
+            return View();
+        }
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            System.Console.WriteLine("came to logout");
+            HttpContext.Session.Clear();
+            return RedirectToAction("results");
+        }
+        [HttpGet("results")]
+        public IActionResult Results()
+            {
+                string gen = HttpContext.Session.GetString("UserName");
+                ViewBag.Gen = gen;
+                int? age_num = HttpContext.Session.GetInt32("Age");
+                ViewBag.Age = age_num;
+                return View();
+            }
+    }
+    }
--- session/Startup.cs
+++ session/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace c_mvc_session
+namespace session
 {
     public class Startup
     {
@@ -28,15 +28,18 @@ namespace c_mvc_session
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            services.AddSession();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc();
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,15 +49,6 @@ namespace c_mvc_session
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
         }
     }
 }
