5ab04cd3924838efbd4c80bae605aab36c6b5469
asingjr2 <asingjr2@gmail.com>
2018-01-25 21:34:01 -0500

    adding dojo_survey simple..

---
+++ dojo_survey/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dojo_survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("Post")]
+        public IActionResult Post(string name, string location, string langauge, string comment)
+        {
+            ViewBag.Name = name;
+            ViewBag.Location = location;
+            ViewBag.Language = langauge; 
+            ViewBag.Comment = comment;
+            return View("Results");
+        }
+
+    }
+}
