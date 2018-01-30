4b37f508a929e3f168e5b71614b67022b5b0c794
asingjr2 <asingjr2@gmail.com>
2018-01-30 00:22:45 -0500

    adding updates to misc for ternary statements

--- c_misc/Program.cs
+++ c_misc/Program.cs
@@ -4,23 +4,22 @@ using System;
 namespace c_misc {
     class Program {
         static void Main (string[] args) {
+            System.Console.WriteLine("***************************************");
             Console.WriteLine ("Misc C# concept practice");
             string words = "some words";
             int nums = 45480;
             bool th = true;
             System.Console.WriteLine($"Nums int variable is {nums} and th bool variable is {th} and type of each is num: {nums.GetType()} and th: {th.GetType()}");
-            try {
-                if (th is int) {
-                    System.Console.WriteLine ("Th is a bool");
-                }
-            } catch {
-                System.Console.WriteLine ("Not working");
-            }
+            System.Console.WriteLine("***************************************");
+            System.Console.WriteLine("Trying Case statements....if code block is blank next block executes...");
+                    // Variable
             switch (nums)
             {
+                // If statement
                 case 50:
                     System.Console.WriteLine("is true");
                     break;
+                // Final else statement
                 default:
                     System.Console.WriteLine("is false");
                     break;
@@ -35,17 +34,11 @@ namespace c_misc {
                     System.Console.WriteLine("is false");
                     break;
             }
-            switch (th)
-            {
-                case false:
-                    System.Console.WriteLine("dang");
-                    break;
-                default:
-                    System.Console.WriteLine("darn");
-                    break;
-            }
             System.Console.WriteLine("***************************************");
-            System.Console.WriteLine("Experimenting with cases and ternary statements");
+            System.Console.WriteLine("Practice with ternary statement which returns  a boolean value only");
+            string tern = nums == 400 ? "400 is corect" : "nums is DEF not 400";
+            System.Console.WriteLine($"The result of ternary true false for nums == 400 is {tern}");
+            System.Console.WriteLine("***************************************");
 
 
 
