813185cc98b295e968efd21884cdce1c4cd53687
asingjr2 <asingjr2@gmail.com>
2018-02-02 23:51:50 -0500

    adding quoting dojo..working

---
+++ quoting_dojo/quoting_dojo/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace quoting_dojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            string allQuotes = "SELECT * FROM quotes";
+            var quotes = DbConnector.Query(allQuotes);
+            if(quotes != null)
+            {
+                ViewBag.AQ = quotes;
+            }
+            else
+            {
+                string noQuotes = "No Quotes Yet!!!!";
+                ViewBag.AQ = noQuotes;
+            }
+            return View();
+        }
+
+        [HttpGet("/quotes")]
+        public IActionResult Quotes()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("/post")]
+        public IActionResult Post(string author, string content, string typee)
+        {
+            switch(typee)
+            {
+                case "form1":
+                    System.Console.WriteLine("this was form1");
+                    break;
+
+                default: 
+                System.Console.WriteLine("this was form2");
+                break;
+            }
+            string addQuote = $"INSERT INTO quotes (author, content) VALUE ('{author}', '{content}', now(), now())";
+            DbConnector.Execute(addQuote);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/errors")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
